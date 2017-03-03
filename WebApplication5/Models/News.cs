e738f01134506e800dcaaea67c0e1f7d9cff9417
Alex Semenov <aleks-boy97@mail.ru>
2017-03-03 14:57:24 +0300

    Add project files.

---
+++ WebApplication5/Models/News.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class News
+    {
+        [Key]
+        public Int32 NewsId { get; set; }
+
+        [Display(Name="Заголовок")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Введите заголовок")]
+        public String Header { get; set; }
+
+        [Display(Name = "Текст новости")]
+        [DataType(DataType.MultilineText)]
+        public String Body { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public String Image { get; set; }
+
+        public DateTime Date { get { return DateTime.Now; } }
+    }
+}
---
+++ WebApplication5/Models/NewsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.IO;
+using System.Data.Entity.Infrastructure;
+
+namespace WebApplication5.Models
+{
+    public class NewsContext : DbContext
+    {
+        public NewsContext()
+            : base("uuuConnection") { }
+
+        public DbSet<News> News { get; set; }
+    }
+}
